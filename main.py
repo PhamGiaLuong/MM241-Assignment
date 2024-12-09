c7b6ed6fd50ffef6b8cba04ed36403995592de02
PhamGiaLuong <pham15032004@gmail.com>
2024-12-09 22:20:15 +0700

    Update code base on gym-cutting-stock v0.0.2 and product rotation

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ env = gym.make(
     "gym_cutting_stock/CuttingStock-v0",
     render_mode="human",  # Comment this line to disable rendering
 )
-NUM_EPISODES = 100
+NUM_EPISODES = 1
 
 if __name__ == "__main__":
     # Reset the environment
@@ -22,8 +22,8 @@ if __name__ == "__main__":
         observation, reward, terminated, truncated, info = env.step(action)
 
         if terminated or truncated:
-            observation, info = env.reset(seed=ep)
             print(info)
+            observation, info = env.reset(seed=ep)
             ep += 1
 
     # Reset the environment
@@ -37,22 +37,22 @@ if __name__ == "__main__":
         observation, reward, terminated, truncated, info = env.step(action)
 
         if terminated or truncated:
-            observation, info = env.reset(seed=ep)
             print(info)
+            observation, info = env.reset(seed=ep)
             ep += 1
 
     # Uncomment the following code to test your policy
-    # # Reset the environment
-    # observation, info = env.reset(seed=42)
-    # print(info)
+    # Reset the environment
+    observation, info = env.reset(seed=42)
+    print(info)
 
-    # policy2210xxx = Policy2210xxx()
-    # for _ in range(200):
-    #     action = policy2210xxx.get_action(observation, info)
-    #     observation, reward, terminated, truncated, info = env.step(action)
-    #     print(info)
+    policy2210xxx = Policy2210xxx(policy_id=1)
+    for _ in range(200):
+        action = policy2210xxx.get_action(observation, info)
+        observation, reward, terminated, truncated, info = env.step(action)
 
-    #     if terminated or truncated:
-    #         observation, info = env.reset()
+        if terminated or truncated:
+            print(info)
+            observation, info = env.reset()
 
 env.close()
